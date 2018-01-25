9814ec3b9f78fc2c250595dab9d322804e43d224
ola <olaclassik@gmail.com>
2018-01-25 15:35:31 +0100

    anonymous functions

---
+++ src/AdvFunctions.kt
@@ -0,0 +1,36 @@
+fun main(args: Array<String>) {
+
+    fun addCache(function: (Int) -> Int): (Int) -> Int {
+        //code
+    }
+
+    val fibonacciNumber: (Int)-> Int = // function implementation
+    val fibonacciNumberWithCache = addCache { fibonacciNumber }
+
+
+    /*val TAG = "MainActivity"
+    val errorHandler = fun(error: Throwable) {
+        if(Build.Config.DEBUG) {
+            Log.e(TAG, error.message, error)
+        }
+        toast(error.message)
+        // Other methods, like: Crashlytics.logException(error).
+    }
+
+    //  Usage in project
+    val adController = AdController(errorHandler)
+    val presenter = MainPresenter(errorHandler)
+
+    //Usage
+    val error = Error("ExampleError")
+    errorHandle(error) // Logs: MainActivity: ExampleError
+*/
+
+
+
+
+
+
+
+
+}
--- src/ReadInput.kt
+++ src/ReadInput.kt
@@ -12,4 +12,118 @@ fun main(args: Array<String>) {
 
     println(customerResponse)
 
+    enum class Color(val r: Int, val g: Int, val b: Int) {
+        BLUE(0, 0, 255),
+        ORANGE(255, 165, 0),
+        GRAY(49, 79, 79),
+        RED(255, 0, 0),
+        VIOLET(238, 130, 238);
+        init {
+            require(r in 0..255)
+            require(g in 0..255)
+            require(b in 0..255)
+        }
+
+        fun rgb() = r shl 16 + g shl 8 + b
+    }
+
+
+    enum class Temperature { COLD, NEUTRAL, WARM }
+
+    enum class Colour(val r: Int, val g: Int, val b: Int) {
+        RED(255, 0,0) {
+            override val temperature = Temperature.WARM
+        },
+        ORANGE(255, 165,0) {
+            override val temperature = Temperature.WARM
+        },
+        BLUE(0,0,255) {
+            override val temperature = Temperature.WARM
+        },
+        GRAY(49, 79,79) {
+            override val temperature = Temperature.WARM
+        },
+        VIOLET(238, 130, 238) {
+            override val temperature = Temperature.WARM
+        };
+
+        init {
+            require(r in 0..256)
+            require(g in 0..256)
+            require(b in 0..256)
+        }
+
+        fun rgb() = (r * 256 + g) * 256 + b
+
+        abstract val temperature: Temperature
+    }
+
+    println(Colour.BLUE.temperature) //prints: COLD
+    println(Colour.ORANGE.temperature) //prints: WARM
+    println(Colour.GRAY.temperature) //prints: NEUTRAL
+
+
+    internal class Fruit private constructor {
+        var weight: Double? = null
+        companion object {
+            fun create() = Fruit()
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
