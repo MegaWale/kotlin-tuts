31bc8db74907848b0549835d63d134a398f6e829
ola <olaclassik@gmail.com>
2018-01-26 21:14:30 +0100

    hashmap (key-value pair) video

--- src/HashMap.kt
+++ src/HashMap.kt
@@ -10,6 +10,20 @@ fun main(args: Array<String>) {
 
     if (myArrayList.contains("James")) println("Son of a Bitch")
 
+    println("\n\n\n")
+    /*
+    * Hashmap = Key-value pairs
+    * */
 
+    var hashmap = HashMap<String, String>()
+    hashmap.put("Mary", "Married")
+    hashmap.put("John", "Married")
+    hashmap.put("John", "Single")
+    hashmap.put("Bob", "Single")
+    hashmap.put("Wale", "Married")
+
+//    println(hashmap.get("John"))
+
+    for (k in hashmap.keys) println(hashmap.get(k))
 
 }
