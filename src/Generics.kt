7055ef5ca66c0195df49fe63a22de7c672cbc645
ola <olaclassik@gmail.com>
2018-01-30 17:59:18 +0100

    variance collections: in and out

--- src/Generics.kt
+++ src/Generics.kt
@@ -5,11 +5,31 @@
 
 fun main(args: Array<String>) {
 
+    class SimpleList<T: Number>
 
+    //Usage
+    var numberList = SimpleList<Number>()
+    var intList = SimpleList<Int>()
+    var studentList = SimpleList<Double>()
+    var stringList = SimpleList<Float>()
 
+    class Action (val name: String)
+    class ActionGroup<T: Action>
 
+    // non-nullable type parameter upper bound
 
+    var actionGroupA: ActionGroup<Action>
+    var actionGroupB: ActionGroup<Action> // error
 
+    class Box<T>
+    open class Animal
+    class Dog: Animal()
+
+    var animalBox = Box<Animal>()
+    var dogBox = Box<Dog>()
+
+    animalBox = dogBox //2, error
+    dogBox = animalBox //1, error
 
 
 
