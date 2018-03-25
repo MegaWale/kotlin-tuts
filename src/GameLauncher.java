c4321787c837ae130c8fad8c0e2da3faa499ef27
olawale <olaclassik@gmail.com>
2018-03-25 15:30:18 +0100

    new dog object java class

--- src/GameLauncher.java
+++ src/GameLauncher.java
@@ -2,5 +2,16 @@ public class GameLauncher {
     public static void main(String[] args) {
         GuessGame game = new GuessGame();
         game.startGame();
+
+        int[] nums;
+        nums = new int[7];
+
+        nums[0] = 6;
+        nums[1] = 19;
+        nums[2] = 44;
+        nums[3] = 42;
+        nums[4] = 10;
+        nums[5] = 20;
+        nums[6] = 1;
     }
 }
